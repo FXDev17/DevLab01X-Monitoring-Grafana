2394700d343b1f0c881c3678a5c8defdc8f79456
FXV <fxvisione@gmail.com>
2025-04-02 11:14:43 +0100

    Fixing Jenkins IAM Role

--- env/dev/infra/IAM/Jenkins-Role.tf
+++ env/dev/infra/IAM/Jenkins-Role.tf
@@ -15,8 +15,23 @@ resource "aws_iam_role" "monitoring_pipeline_Role" {
   assume_role_policy = data.aws_iam_policy_document.monitoring_pipeline_Assume_Role_Policy.json
 }
 
-# Defininf the IAM Policy
+# Defining the IAM Policy Document
 data "aws_iam_policy_document" "monitoring_pipeline_Permissions_Policy" {
+  # S3 Permissions (no EC2-specific condition)
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "arn:aws:s3:::devlab00-logging",
+      "arn:aws:s3:::devlab00-logging/*"
+    ]
+  }
+
+  # EC2 and IAM Permissions (with condition)
   statement {
     actions = [
       # EC2 Permissions
@@ -25,35 +40,22 @@ data "aws_iam_policy_document" "monitoring_pipeline_Permissions_Policy" {
       "ec2:StopInstances",
       "ec2:ImportKeyPair",
       "ec2:CreateSecurityGroup",
-
-      # S3 Permissions
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-
       # IAM Permissions
       "iam:CreateRole",
       "iam:CreatePolicy"
     ]
     resources = [
-      # S3 resources
-      "arn:aws:s3:::devlab00-logging",
-      "arn:aws:s3:::devlab00-logging/*",
-
       # EC2 resources
       "arn:aws:ec2:eu-west-1:817520395860:key-pair/*",
       "arn:aws:ec2:eu-west-1:817520395860:security-group/*",
-
       # IAM resources
       "arn:aws:iam::817520395860:role/monitoring_pipeline_Role/*",
       "arn:aws:iam::817520395860:policy/monitoring_pipeline_Policy/*"
-    
     ]
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "ec2:ResourceTag/ManagedBy"
-      values = [ "jenkins" ]
+      values   = ["jenkins"]
     }
   }
 }
